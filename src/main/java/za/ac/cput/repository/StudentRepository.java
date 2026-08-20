b8183c33759db9061c95a6355e6f97555f601c32
samthepessimist <231116144@mycput.ac.za>
2026-08-20 21:51:20 +0200

    updated student repo with new method

--- src/main/java/za/ac/cput/repository/StudentRepository.java
+++ src/main/java/za/ac/cput/repository/StudentRepository.java
@@ -3,6 +3,10 @@ package za.ac.cput.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import za.ac.cput.domain.Student;
+
+import java.util.Optional;
+
 @Repository
 public interface StudentRepository extends JpaRepository<Student, String> {
+    Optional<Student> findByStudentNumber(String studentNumber);
 }
